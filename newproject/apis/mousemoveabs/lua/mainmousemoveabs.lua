b02617c1e719b819c085d1dcfc8a412f1bda7dcd
viografiko <lol924111@gmail.com>
2024-12-25 15:02:06 -0300

    Update mainmousemoveabs.lua

--- newproject/apis/mousemoveabs/lua/mainmousemoveabs.lua
+++ newproject/apis/mousemoveabs/lua/mainmousemoveabs.lua
@@ -1,1 +1,25 @@
+local HttpService = game:GetService("HttpService")
 
+local function mousemoveabs(x, y)
+	local url = "http://127.0.0.1:5000/mousemoveabs"  
+	local data = {
+		x = x,
+		y = y
+	}
+
+	local success, response = pcall(function()
+		return HttpService:PostAsync(url, HttpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
+	end)
+
+	if success then
+		--print("200 sucess:", response)
+	else
+		warn("404 error:", response)
+	end
+end
+
+--[[
+while wait(1) do
+	mousemoveabs(500, 300)
+end
+]]
