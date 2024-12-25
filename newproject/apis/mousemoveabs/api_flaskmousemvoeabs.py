0d543709e86b3e0e7e0e166cbb8755ca0772c0a2
viografiko <lol924111@gmail.com>
2024-12-25 15:00:35 -0300

    Update api_flaskmousemvoeabs.py

--- newproject/apis/mousemoveabs/api_flaskmousemvoeabs.py
+++ newproject/apis/mousemoveabs/api_flaskmousemvoeabs.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, jsonify
+import pyautogui
 
+app = Flask(__name__)
+
+@app.route('/mousemoveabs', methods=['POST'])
+def mouse_move_abs():
+    try:
+        data = request.json
+        x = data.get("x", 0)
+        y = data.get("y", 0)
+        pyautogui.moveTo(x, y) 
+        return jsonify({"status": "success", "message": f"sucess 200 ({x}, {y})"}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+    
+@app.route('/')
+def home():
+    return '200'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
