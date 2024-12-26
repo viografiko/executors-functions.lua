0e0d382724aca733ace0d3a5a1384f49701e871b
viografiko <lol924111@gmail.com>
2024-12-26 08:39:01 -0300

    Update main_scroll.lua

--- newproject/apis/mousescroll/lua/main_scroll.lua
+++ newproject/apis/mousescroll/lua/main_scroll.lua
@@ -3,7 +3,6 @@
 AND USE A HTTPLIB.
 ]]
 local HttpService = game:GetService("HttpService")
-local plr = game.Players.LocalPlayer
 
 local function mousescroll(number)
 	local url = "http://127.0.0.1:5000/mousescroll"
